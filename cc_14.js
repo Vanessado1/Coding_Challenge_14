b3d2f091d395e42c07c06c3a0d8ad3df79323606
Vanessa Do <xex.vanessa@gmail.com>
2025-03-09 16:57:29 -0400

    Task 3- highlighting high priority tickets

--- cc_14.js
+++ cc_14.js
@@ -2,6 +2,10 @@
 function supportTicket(name, description, priorityLevel) {
     const ticket = document.createElement('div'); // create main ticket div 
     ticket.setAttribute('class', 'support-ticket');
+    // high priority class identifier 
+    if (priorityLevel === 'High') {
+        ticket.classList.add('high-priority');
+    }
     // create and append customer name heading 
     const nameHeading = document.createElement('h2');
     nameHeading.textContent = name;
@@ -25,8 +29,23 @@ function supportTicket(name, description, priorityLevel) {
     const ticketContainer = document.getElementById('ticketContainer');
     ticketContainer.appendChild(ticket);
 }
+// Task 3 
+function highlightHighPriorityTickets() {
+    // selects all high priority tickets
+    const highPriorityTickets = document.querySelectorAll('.high-priority');
+    // convert NodeList to an array
+    const highPriorityArray = Array.from(highPriorityTickets);
+    // updates high priority tickets 
+    highPriorityArray.forEach(ticket => {
+        ticket.computedStyleMap.backgroundColor = 'red';
+    });
+}
+
 // list all customer support tickets 
 supportTicket('Alice Johnson', 'Unable to reset password', 'High');
 supportTicket('Bob Willy', 'Website is loading slowly', 'Medium');
 supportTicket('Chris Brown', 'Error message when submitting forms', 'Low');
 supportTicket('Danny Patterson', 'Feature request: Dark Mode', 'Medium');
+
+// calls the function 
+highlightHighPriorityTickets();
